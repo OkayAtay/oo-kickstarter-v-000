a1a7e7d5ec8aee8430e59bbdabd7ee64bf58a321
Araba Taylor <araba.taylor20@gmail.com>
2017-11-15 03:31:29 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
   end
 
   def add_backer(backer)
-          binding.pry
+    binding.pry
     @backers << backer
     if self.backers.each {|project| project.backed_projects != self }
 
