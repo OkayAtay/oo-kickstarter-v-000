42f1cb5861d95c89f8c4297e25f04a409985c921
Araba Taylor <araba.taylor20@gmail.com>
2017-11-15 03:18:04 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,6 +11,6 @@ attr_accessor :name, :backed_projects
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self) unless
+    project.add_backer(self) unless @backers.include?(self)
   end
 end
